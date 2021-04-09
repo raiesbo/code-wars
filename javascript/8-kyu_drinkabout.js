0bccb9ad6335353e3bde3c0e4adb63382ab702ff
raiesbo <raiesbo8@gmail.com>
2021-04-10 00:18:05 +0200

    kata js

---
+++ javascript/8-kyu_drinkabout.js
@@ -0,0 +1,26 @@
+// Drink about
+
+
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+const peopleWithAgeDrink = (old) => {
+    return old < 14 ? "drink toddy" : old >= 14 && old < 18 ? "drink coke" : old >= 18 && old < 21 ? "drink beer" : "drink whisky"
+};
