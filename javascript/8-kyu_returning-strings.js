03e6bb478a1e33acea1ac20f45ba9b0b7e1fa63d
Raimon Espasa <raimon.espasa.bou@webgears-group.com>
2022-06-15 08:42:11 +0200

    Resolve new js kata

---
+++ javascript/8-kyu_returning-strings.js
@@ -0,0 +1,11 @@
+// 8 kyu // Returning Strings
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+const greet = name => `Hello, ${name} how are you doing today?`;
+
+
+greet("Ryan") === "Hello, Ryan how are you doing today?" ? true : false
+greet("Shingles") === "Hello, Shingles how are you doing today?" ? true : false
